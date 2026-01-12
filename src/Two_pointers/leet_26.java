e4ba6823a5c8f36871f600fa7d63080f4929dbaf
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-01-13 00:14:59 +0530

    leetcode 26

---
+++ src/Two_pointers/leet_26.java
@@ -0,0 +1,24 @@
+package Two_pointers;
+
+import Arrays_Hashing.leet_643;
+
+public class leet_26 {
+     public int removeDuplicates(int [] nums){
+        if (nums == null) return 0;
+
+        int slow = 0;
+        for (int fast=1; fast<= nums.length-1; fast++){
+            if (nums[slow] != nums[fast]){
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow+1;
+     }
+    public static void main(String[] args) {
+        leet_26 obj  = new leet_26();
+        int [] arr = {0,0,1,1,1,2,2,3,3,4};
+        int ans = obj.removeDuplicates(arr);
+        System.out.println("ans : " + ans);
+    }
+}
