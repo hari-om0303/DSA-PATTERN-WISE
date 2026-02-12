afda5fa43b236f39b49008cbeec9dfecae45607f
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-02-12 10:31:33 +0530

    Remove Nodes From Linked List

---
+++ src/Stack/leet_2487.java
@@ -0,0 +1,40 @@
+package Stack;
+
+public class leet_2487 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNodes(ListNode head) {
+        Stack<ListNode> st = new Stack<>();
+        ListNode curr = head;
+       while (curr != null){
+           while (!st.isEmpty() && st.peek().val<curr.val){
+               st.pop();
+           }
+           st.push(curr);
+           curr = curr.next;
+       }
+        ListNode next = null;
+       while (!st.isEmpty()){
+           ListNode newnode = st.pop();
+           newnode.next = next;
+           next = newnode;
+       }
+       return  next;
+    }
+}
