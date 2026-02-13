d0d0d6edad0ffb65dd6a8672f3bae3e617fa0cfb
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-02-13 20:54:39 +0530

    first unit character in a string

---
+++ src/HashMaps__/Leet_387.java
@@ -0,0 +1,23 @@
+package HashMaps__;
+
+import java.util.HashMap;
+
+public class Leet_387 {
+    public static int firstUniqChar(String s) {
+        HashMap<Character , Integer> map = new HashMap<>();
+        for (int i =0 ; i<s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch , map.getOrDefault(ch , 0)+1);
+        }
+        for (int i=0 ; i<s.length(); i++){
+            if (map.get(s.charAt(i)) == 1 ){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("lovwleetcode"));
+    }
+}
