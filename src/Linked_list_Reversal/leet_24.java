257a4581522680023f488a56d6a8a91d85655bef
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-02-14 16:32:04 +0530

    swap node in pair

---
+++ src/Linked_list_Reversal/leet_24.java
@@ -0,0 +1,35 @@
+package Linked_list_Reversal;
+
+public class leet_24 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+             ListNode dummy = new ListNode(0);
+             dummy.next = head;
+             ListNode prev = dummy;
+             while (prev.next != null && prev.next.next!= null){
+                 ListNode first = prev.next;
+                 ListNode second = prev.next.next;
+                 first.next = second.next;
+                 second.next =first;
+                 prev.next = second;
+                 prev = first;
+             }
+     return dummy.next;
+    }
+}
